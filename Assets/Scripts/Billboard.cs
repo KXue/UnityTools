0b64620eea60fe8a7ac84eb86755e998237ea098
Kevin Xue <ivexnuke@gmail.com>
2018-08-28 12:41:32 -0400

    Added billboard script

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class Billboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float cameraY = Camera.main.transform.rotation.eulerAngles.y;
+        float cameraX = Camera.main.transform.rotation.eulerAngles.x;
+        Vector3 newRotation = transform.rotation.eulerAngles;
+        newRotation.y = cameraY;
+        newRotation.x = cameraX;
+        transform.rotation = Quaternion.Euler(newRotation);
+	}
+}
