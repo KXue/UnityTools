c184683308f820f995cbe7f10b20e1cc5cf8deeb
Kevin Xue <ivexnuke@gmail.com>
2018-08-30 18:35:30 -0400

    Cleaned up 3D movemment and camera controls

---
+++ Assets/Scripts/CameraController3D.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController3D : MonoBehaviour {
+	public float m_mouseSensitivity;
+	public float m_blockOffsetDistance; 
+	public Vector3 m_cameraFocus;
+	public Vector3 m_cameraOffset;
+	public Vector3 m_pivotOffset;
+	public Quaternion m_pivotRotation;
+	public LayerMask m_viewBlockinglayer;
+	private Transform m_cameraTransform;
+	private Vector3 m_cameraPosition;
+	private Vector3 m_cameraFocalPoint;
+	// Use this for initialization
+	void Start () {
+		m_cameraTransform = Camera.main.transform;
+		m_pivotRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float yRot = Input.GetAxis("Mouse X") * (m_mouseSensitivity * Time.deltaTime);
+        float xRot = Input.GetAxis("Mouse Y") * (m_mouseSensitivity * Time.deltaTime);
+
+
+        transform.rotation *= Quaternion.Euler(0f, yRot, 0f);
+		m_pivotRotation *= Quaternion.Euler(-xRot, yRot, 0f);
+
+		MoveCamera();
+	}
+
+	void MoveCamera(){
+		Vector3 worldPivotPoint = transform.position + m_pivotOffset;
+		Vector3 worldFocalPoint = worldPivotPoint + m_pivotRotation * m_cameraFocus;
+		Vector3 worldCameraPoint = worldPivotPoint + m_pivotRotation * m_cameraOffset;
+		Vector3 lookDirection = (worldFocalPoint - worldCameraPoint).normalized;
+		RaycastHit hitInfo;
+		
+		if(Physics.Linecast(worldFocalPoint, worldCameraPoint, out hitInfo, m_viewBlockinglayer)){
+			worldCameraPoint = hitInfo.point + lookDirection.normalized * m_blockOffsetDistance;
+		}
+		m_cameraTransform.position = worldCameraPoint;
+		m_cameraTransform.LookAt(worldFocalPoint);
+		m_cameraFocalPoint = worldFocalPoint;
+		m_cameraPosition = worldCameraPoint;
+	}
+	void OnDrawGizmosSelected(){
+		Gizmos.color = new Color(0, 1, 0, 1);
+		Gizmos.DrawLine(m_cameraPosition, m_cameraFocalPoint);
+	}
+
+}
---
+++ Assets/Scripts/MovementController3D.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class MovementController3D : MonoBehaviour {
+	private const float EPSILON = 0.1f;
+	public float m_walkSpeed = 5;
+	public float m_jumpSpeed = 10;
+	public float m_gravityMultiplier = 2;
+	public float m_groundStickVelocity = -10;
+	public LayerMask m_groundLayer;
+	bool m_isJumping;
+	Vector2 m_inputPlane;
+	Vector3 m_worldVelocity;
+	bool m_onSlope;
+
+	private CharacterController m_characterController;
+
+	// Use this for initialization
+	void Start () {
+		m_inputPlane = new Vector2();
+		m_worldVelocity = new Vector3();
+		m_characterController = GetComponent<CharacterController>();
+	}
+	void Update(){
+		if(!m_isJumping && m_characterController.isGrounded){
+			m_isJumping = Input.GetButtonDown("Jump");
+		}
+	}
+	void FixedUpdate () {
+		UpdateVelocity();
+        m_characterController.Move(m_worldVelocity * Time.fixedDeltaTime);
+		m_worldVelocity = m_characterController.velocity;
+    }
+	void UpdateVelocity(){
+		GetInput();
+		UpdateXZVelocity();
+		UpdateYVelocity();
+	}
+	void GetInput(){
+        m_inputPlane.x = Input.GetAxisRaw("Horizontal");
+		m_inputPlane.y = Input.GetAxisRaw("Vertical");
+		if(m_inputPlane.sqrMagnitude > 1){
+            m_inputPlane.Normalize();
+		}
+	}
+	void UpdateXZVelocity(){
+		Vector3 desiredMove = transform.forward * m_inputPlane.y + transform.right * m_inputPlane.x;
+		m_worldVelocity.x = desiredMove.x * m_walkSpeed;
+		m_worldVelocity.z = desiredMove.z * m_walkSpeed;
+		if(m_characterController.isGrounded){
+			RaycastHit hitInfo;
+			if(Physics.SphereCast(transform.position, m_characterController.radius, -transform.up, out hitInfo, 
+				m_characterController.height * 0.5f + m_characterController.radius + m_characterController.skinWidth, m_groundLayer)){
+				m_worldVelocity = Vector3.ProjectOnPlane(m_worldVelocity, hitInfo.normal);
+			}
+		}
+	}
+	void UpdateYVelocity(){
+		if (m_isJumping && m_characterController.isGrounded)
+        {
+            m_worldVelocity.y = m_jumpSpeed;
+            m_isJumping = false;
+        }
+		else if(m_characterController.isGrounded && m_worldVelocity.y > 0){
+			m_worldVelocity.y = 0;
+		}
+		else{
+			m_worldVelocity += Physics.gravity * m_gravityMultiplier * Time.fixedDeltaTime;
+		}
+    }
+}
